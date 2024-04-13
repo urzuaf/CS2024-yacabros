b94f81e7d94aa2676932e3cafad06f3a3db839ba
Claudio <clhernandez21@alumnos.utalca.cl>
2024-04-13 15:53:34 -0400

    creacion front creartorneos y consulta para pedir torneos

--- Avances/Hernandez.md
+++ Avances/Hernandez.md
@@ -4,3 +4,8 @@
 16:08 - inicio implem - 17:36 pausa
 18:55 -  retomar -- 22.25 fin
 22.37 - retomar  -- 23.04 fin
+
+
+------------------------------
+10:56 inicio vista creacion 11:47 pausa
+14.42 retomar
---
+++ FRONTEND/src/Componentes/TableTorneos.svelte
@@ -0,0 +1,53 @@
+<script>
+    let rows;
+
+    async function getData() {
+        console.log("holaA")
+        try{
+            console.log("hola")
+            const resp = await fetch("http://localhost:3000/getTorneos");
+            if (!resp.ok)
+                throw new Error("Error al obtener los datos del servidor");
+
+            rows = await resp.json();
+            console.log("Datos recibidos:", rows);
+
+        }catch(error){
+            console.log("Error: ",error);
+        }
+    }
+
+    window.onload = getData;
+    
+
+</script>
+
+
+
+
+  
+<div>
+    <table class="table-auto">
+        <thead>
+          <tr>
+            <th class="px-4 py-2">Id</th>
+            <th class="px-4 py-2">Creador</th>
+            <th class="px-4 py-2">Nombre</th>
+            <th class="px-4 py-2">Deporte</th>
+            <th class="px-4 py-2">Formato</th>
+            <th class="px-4 py-2">Fecha de Inicio</th>
+            <th class="px-4 py-2">Fecha de Cierre</th>
+          </tr>
+        </thead>
+
+        <tbody id=data>
+          <tr>
+            <td class="border px-4 py-2">Intro to CSS</td>
+            <td class="border px-4 py-2">Adam</td>
+            <td class="border px-4 py-2">858</td>
+          </tr>
+
+        </tbody>
+      </table>
+    
+</div>
