1c5e6da2160064eb69016e7322a18877793b28df
Vejat Olea <volea19@alumnos.utalca.cl>
2024-04-23 11:48:11 -0400

    4/23

--- FRONTEND/src/Componentes/Modal.svelte
+++ FRONTEND/src/Componentes/Modal.svelte
@@ -1,6 +1,14 @@
 <script>
   let bases = '';
   let id = 1;
+  let nombre= '';
+  let deporte = '';
+  let fechaInicio = '';
+  let fechaFin = '';
+  let etiquetas ='';
+  let pricePool = '';
+  let formato = '';
+
 
   let showModal;
   let dialog;
@@ -14,7 +22,7 @@
               headers: {
                   'Content-Type': 'application/json'
               },
-              body: JSON.stringify({ id, bases })
+              body: JSON.stringify({ id, nombre, deporte, fechaInicio, fechaFin, formato, etiquetas, pricePool })
           });
           if (!response.ok){
               throw new Error("Error al agregar torneo");
@@ -50,32 +58,52 @@
   <div class="flex justify-center items-center w-full h-full">
       <form on:submit|preventDefault={handleSubmit} class="flex flex-col gap-4 my-4 w-3/4 md:w-3/5  rounded-lg p-4">
           <div class="mb-3">
-            <label for="nombre" class="block text-sm font-medium text-zinc-400">Nombre del Torneo:</label>
+            <label for="nombre" class="block text-sm font-medium  text-gray-700">Nombre del Torneo:</label>
             <input type="text" id="nombre" v-model="nombre" required
-                class="block w-full px-4 py-4 mt-1 text-lg text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
-        </div>
+                class="block w-full px-4 py-4 mt-1 text-lg text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                bind:value={nombre}
+            />
+               
+            
+                
+            </div>
         <div class="mb-3">
             <label for="deporte" class="block text-sm font-medium text-gray-700">Deporte:</label>
             <input type="text" id="deporte" v-model="deporte" required
-                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                bind:value={deporte}
+            />
         </div>
         <div class="mb-3">
             <label for="fechaInicio" class="block text-sm font-medium text-gray-700">Fecha de Inicio:</label>
             <input type="date" id="fechaInicio" v-model="fechaInicio" required
-                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                bind:value={fechaInicio}
+            />
         </div>
         <div class="mb-3">
             <label for="fechaFin" class="block text-sm font-medium text-gray-700">Fecha de Término:</label>
             <input type="date" id="fechaFin" v-model="fechaFin" required
-                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                bind:value={fechaFin}
+            />
         </div>
         <div class="mb-3">
             <label for="formato" class="block text-sm font-medium text-gray-700">Formato del Torneo:</label>
             <select id="formato" v-model="formato"
-                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                bind:value={formato}>
                 <option value="bracket">Bracket</option>
                 <option value="tabla">Tabla</option>
+                
             </select>
+                
+            
+                
+                
+            
+                
+            
         </div>
         <div class="mb-3">
             <label for="equipos" class="block text-sm font-medium text-gray-700">Cantidad de Equipos:</label>
@@ -85,16 +113,31 @@
         <div class="mb-3">
             <label for="etiquetas" class="block text-sm font-medium text-gray-700">Etiquetas:</label>
             <input type="text" id="etiquetas" v-model="etiquetas"
-                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                bind:value={etiquetas}
+            />
         </div>
         <div class="mb-3">
             <label for="pricePool" class="block text-sm font-medium text-gray-700">Price Pool:</label>
             <input type="text" id="pricePool" v-model="pricePool"
-                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                bind:value={pricePool}
+            />
         </div>
-        <div class="mt-2">
-            <button class="w-auto px-8 py-4 text-sm font-medium tracking-wide text-white capitalize transition-all duration-300 transform bg-green-600 rounded-lg hover:scale-105 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-                Crear Torneo
+        <div class="w-6/5 flex justify-center md:justify-evenly flex-wrap gap-3">
+            <button
+                on:click={() => dialog.close()}
+                type="submit" aria-label="save bases modal"
+                class="w-4/5 md:w-2/5 min-w-fit px-4 py-2 font-medium text-white trans bg-green-600 rounded-lg hover:scale-105"
+            >
+                Guardar
+            </button>
+            <button 
+                on:click={() => dialog.close()}
+                type="button" aria-label="close bases modal"
+                class="w-4/5 md:w-2/5 min-w-fit px-4 py-2 font-medium text-white trans bg-green-600 rounded-lg hover:scale-105"
+            >
+                Cancelar
             </button>
         </div>         
       </form>
