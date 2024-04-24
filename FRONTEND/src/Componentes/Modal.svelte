e8e08347692f2e1cec7ace7c2d9af8d5014c9481
Nico <ncuadra19@alumnos.utalca.cl>
2024-04-23 23:42:23 -0400

    Más cambios en colores de páginas con uso de la paleta de colores

--- FRONTEND/src/Componentes/Modal.svelte
+++ FRONTEND/src/Componentes/Modal.svelte
@@ -28,16 +28,11 @@
     }
   </script>
   
-  <!-- 
-    Añadir a página como:
-    <FormBases client:load/> 
-  -->
-  
   <!-- Botón para mostrar el Modal -->
   <button 
     on:click={()=>{showModal = true}} 
     type="button" aria-label="toggle bases modal"
-    class="w-auto px-4 py-2 font-medium text-white trans bg-green-600 rounded-lg hover:scale-105"
+    class="w-auto px-4 py-2 font-medium text-dark-text trans bg-sportify rounded-lg hover:scale-105"
   >
     Modificar Bases
   </button>
@@ -46,33 +41,33 @@
   <dialog 
     bind:this={dialog}
     on:close={() => (showModal = false)}
-    class="w-auto h-70  bg-gray-100 border-green-500 rounded-lg ">
+    class="w-auto h-70 rounded-lg bg-transparent">
     <div class="flex justify-center items-center w-full h-full">
-        <form on:submit|preventDefault={handleSubmit} class="flex flex-col gap-4 my-4 w-3/4 md:w-3/5  rounded-lg p-4">
+        <form on:submit|preventDefault={handleSubmit} class="flex flex-col gap-4 my-4 w-3/4 md:w-3/5 bg-light-background dark:bg-dark-background  rounded-lg p-4">
             <div class="mb-3">
               <label for="nombre" class="block text-sm font-medium text-zinc-400">Nombre del Torneo:</label>
               <input type="text" id="nombre" v-model="nombre" required
-                  class="block w-full px-4 py-4 mt-1 text-lg text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                  class="block w-full px-4 py-4 mt-1 text-lg text-light-text dark:text-dark-text bg-light-input dark:bg-dark-input border border-sportify rounded-lg focus:ring-sportify focus:outline-none focus:ring focus:ring-opacity-40">
           </div>
           <div class="mb-3">
               <label for="deporte" class="block text-sm font-medium text-gray-700">Deporte:</label>
               <input type="text" id="deporte" v-model="deporte" required
-                  class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                  class="block w-full px-4 py-4 mt-1 text-lg text-light-text dark:text-dark-text bg-light-input dark:bg-dark-input border border-sportify rounded-lg focus:ring-sportify focus:outline-none focus:ring focus:ring-opacity-40">
           </div>
           <div class="mb-3">
               <label for="fechaInicio" class="block text-sm font-medium text-gray-700">Fecha de Inicio:</label>
               <input type="date" id="fechaInicio" v-model="fechaInicio" required
-                  class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                  class="block w-full px-4 py-4 mt-1 text-lg text-light-text dark:text-dark-text bg-light-input dark:bg-dark-input border border-sportify rounded-lg focus:ring-sportify focus:outline-none focus:ring focus:ring-opacity-40">
           </div>
           <div class="mb-3">
               <label for="fechaFin" class="block text-sm font-medium text-gray-700">Fecha de Término:</label>
               <input type="date" id="fechaFin" v-model="fechaFin" required
-                  class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                  class="block w-full px-4 py-4 mt-1 text-lg text-light-text dark:text-dark-text bg-light-input dark:bg-dark-input border border-sportify rounded-lg focus:ring-sportify focus:outline-none focus:ring focus:ring-opacity-40">
           </div>
           <div class="mb-3">
               <label for="formato" class="block text-sm font-medium text-gray-700">Formato del Torneo:</label>
               <select id="formato" v-model="formato"
-                  class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                  class="block w-full px-4 py-4 mt-1 text-lg text-light-text dark:text-dark-text bg-light-input dark:bg-dark-input border border-sportify rounded-lg focus:ring-sportify focus:outline-none focus:ring focus:ring-opacity-40">
                   <option value="bracket">Bracket</option>
                   <option value="tabla">Tabla</option>
               </select>
@@ -80,20 +75,20 @@
           <div class="mb-3">
               <label for="equipos" class="block text-sm font-medium text-gray-700">Cantidad de Equipos:</label>
               <input type="number" id="equipos" v-model="equipos" required
-                  class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                  class="block w-full px-4 py-4 mt-1 text-lg text-light-text dark:text-dark-text bg-light-input dark:bg-dark-input border border-sportify rounded-lg focus:ring-sportify focus:outline-none focus:ring focus:ring-opacity-40">
           </div>
           <div class="mb-3">
               <label for="etiquetas" class="block text-sm font-medium text-gray-700">Etiquetas:</label>
               <input type="text" id="etiquetas" v-model="etiquetas"
-                  class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                  class="block w-full px-4 py-4 mt-1 text-lg text-light-text dark:text-dark-text bg-light-input dark:bg-dark-input border border-sportify rounded-lg focus:ring-sportify focus:outline-none focus:ring focus:ring-opacity-40">
           </div>
           <div class="mb-3">
               <label for="pricePool" class="block text-sm font-medium text-gray-700">Price Pool:</label>
               <input type="text" id="pricePool" v-model="pricePool"
-                  class="block w-full px-4 py-4 mt-1 text-sm text-gray-700 bg-white border border-green-500 rounded-lg focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                  class="block w-full px-4 py-4 mt-1 text-lg text-light-text dark:text-dark-text bg-light-input dark:bg-dark-input border border-sportify rounded-lg focus:ring-sportify focus:outline-none focus:ring focus:ring-opacity-40">
           </div>
           <div class="mt-2">
-              <button class="w-auto px-8 py-4 text-sm font-medium tracking-wide text-white capitalize transition-all duration-300 transform bg-green-600 rounded-lg hover:scale-105 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+              <button class="w-auto px-8 py-4 text-sm font-medium text-dark-text transition-all duration-300 transform bg-sportify rounded-lg hover:scale-105 focus:outline-none focus:ring focus:ring-sportify focus:ring-opacity-50">
                   Crear Torneo
               </button>
           </div>         
