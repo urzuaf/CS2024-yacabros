d881530dc9e2bb340250d881026660dfa3d0c403
Vejat Olea <volea19@alumnos.utalca.cl>
2024-04-24 22:04:10 -0400

    fin 24/4

--- FRONTEND/src/Componentes/Profile.svelte
+++ FRONTEND/src/Componentes/Profile.svelte
@@ -1,10 +1,34 @@
 <script>
     import { Usuario } from "../stores/login_store";
+    import { onMount } from 'svelte';
     let isOpen = false;
     let userProfile = {
-        name: "Daniel Moreno",
+        name: "",
         email: $Usuario
     };
+
+    onMount(async () =>{
+        try{
+            const resp = await fetch("http://localhost:3000/getUsername", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: userProfile.email }), // Envía el correo electrónico en el cuerpo de la solicitud
+            });
+            if (!resp.ok)
+                throw new Error("Error al obtener los datos del servidor");
+
+            const result = await resp.json();
+            
+            if (result.length > 0) {
+                userProfile.name = result[0].username; 
+            }
+
+        }catch(error){
+            console.log("Error: ",error);
+        }
+    });
 </script>
 
 
@@ -24,26 +48,25 @@
     </div>
     <h3 class="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">{userProfile.name}</h3>
 </button>
+-->
+
+{#if isOpen}
+    {#if $Usuario != ''}
+        <div class="absolute right-0 top-10 bg-white p-2 rounded shadow">
+            <!-- Nombre de usuario y correo -->
+            <p class="text-gray-800 font-bold">{userProfile.name}</p>
+            <p class="text-gray-300">{userProfile.email}</p>
+
+            <!-- Botón para editar perfil -->
+            <a href="/editData" class="block w-full text-left py-2 px-4 text-gray-800 hover:bg-gray-100 focus:outline-none">
+                Editar Perfil
+            </a>
 
 
-{#if $Usuario !="" && isOpen}
-
-    <div class="absolute right-0 top-10 bg-white p-2 rounded shadow">
-       
-        <p class="text-ray-800 font-bold">{userProfile.name}</p>
-        <p class="text-gray-300">{userProfile.email}</p>
-
-        <!-- Botón para editar perfil -->
-        <a href="/editData" class="block w-full text-left py-2 px-4 text-gray-800 hover:bg-gray-100 focus:outline-none">
-
-
-            Editar Perfil
-        </a>
-        
-    </div>
-
+        </div>
     {:else}
         <div class="absolute right-0 top-10 bg-white p-2 rounded shadow">
+            
             <!-- Botón para editar perfil -->
             <a href="/login" class="block w-full text-left py-2 px-4 text-gray-800 hover:bg-gray-100 focus:outline-none">
                 Iniciar sesión
@@ -51,9 +74,8 @@
             <a href="/register" class="block w-full text-left py-2 px-4 text-gray-800 hover:bg-gray-100 focus:outline-none">
                 Registrarse
             </a>
-            <!-- Otros botones aquí -->
         </div>
-
+    {/if}
 {/if}
  -->
 
