25541af644070c879d09482de1d731000da8d6e5
Notand105 <urzua.fernando.f@gmail.com>
2024-05-06 08:39:42 -0400

    click para desplazar en bracket

--- FRONTEND/src/Componentes/Bracket.svelte
+++ FRONTEND/src/Componentes/Bracket.svelte
@@ -1,23 +1,51 @@
 <script>
- import {bracketData8, bracketData4, bracketData2} from '../stores/bracketsmockdata.js'
+ import {bracketData8, bracketData4, bracketData2, bracketData16} from '../stores/bracketsmockdata.js'
 
- let bracketData = bracketData2
+ let bracketData = bracketData16
 
-    console.log(bracketData)
+ const handleClick = (indexParent, index) => {
+        if (indexParent == bracketData.length) {
+            return;
+        }
+
+        //obtenemos el indice a agregar en el siguiente
+        let indice_a_agregar = 0;
+        if (index % 2 == 0) {
+            indice_a_agregar = index / 2;
+        } else {
+            indice_a_agregar = (index - 1) / 2;
+        }
+
+        // agregamos al siguiente bracket
+        let newBracketData = bracketData;
+
+        newBracketData[indexParent + 1][indice_a_agregar].name =
+            bracketData[indexParent][index].name;
+        newBracketData[indexParent + 1][indice_a_agregar].result = 0;
+        bracketData = [...newBracketData];
+    };
+
 </script>
 
-<h1 class="text-center font-semibold p-4 mb-10">Bracket de torneos</h1>
+<h1 class="text-center font-semibold">Bracket de torneo</h1>
+<p class="text-center mb-10">Haz click sobre el equipo ganador</p>
 <div class="w-screen pb-8">
     <div class="flex w-9/12 m-auto gap-20 justify-around">
         {#each bracketData as etapa, parentIndex}
-            <div class="flex flex-col gap-8 justify-around">
+            <div class="flex flex-col gap-4 justify-around">
                 {#each etapa as equipo, index}
                     <div class=" flex items-center gap-4">
-                        <div class="flex flex-col gap-2">
-                            <div
+                        <div class:impar = {index %2 !=0 }
+                        class="flex flex-col gap-2">
+                            <button
+                                class:winner={parentIndex ==
+                                    bracketData.length - 1}
+                                title="seleccionar como ganador"
+                                on:click={() => handleClick(parentIndex, index)}
+                                class="border p-2 shadow-md rounded"
                             >
                                 {equipo.name}
-                            </div>
+                            </button>
                         </div>
                     </div>
                 {/each}
@@ -25,3 +53,9 @@
         {/each}
     </div>
 </div>
+
+<style>
+    .impar{
+        margin: 0px 0px 24px 0px;
+    }
+</style>
--- FRONTEND/src/stores/bracketsmockdata.js
+++ FRONTEND/src/stores/bracketsmockdata.js
@@ -1,3 +1,46 @@
+let bracketData16 = [
+    [
+        { name: "Leones Negros", result: 0 },
+        { name: "Aguilas Doradas", result: 0 },
+        { name: "Tigres Blancos", result: 0 },
+        { name: "Lobos Rojos", result: 0 },
+        { name: "Panteras Azules", result: 0 },
+        { name: "Halcones Plateados", result: 0 },
+        { name: "Huachipato", result: 0 },
+        { name: "Pumas Carmesí", result: 0 },
+        { name: "Leones Negros", result: 0 },
+        { name: "Aguilas Doradas", result: 0 },
+        { name: "Tigres Blancos", result: 0 },
+        { name: "Lobos Rojos", result: 0 },
+        { name: "Panteras Azules", result: 0 },
+        { name: "Halcones Plateados", result: 0 },
+        { name: "Huachipato", result: 0 },
+        { name: "Pumas Carmesí", result: 0 },
+    ],
+    [
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+    ],
+    [
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+    ],
+    [
+        { name: "--------", result: 0 },
+        { name: "--------", result: 0 },
+    ],
+
+    [{ name: "Ganador", result: 0 }],
+];
+
 let bracketData8 = [
     [
         { name: "Leones Negros", result: 0 },
@@ -45,5 +88,5 @@ let bracketData2 = [
 ];
 
 export {
-    bracketData8, bracketData4, bracketData2
+    bracketData8, bracketData4, bracketData2, bracketData16
 }
