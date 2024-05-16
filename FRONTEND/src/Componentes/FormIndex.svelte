d46b9709a4c5fe812a92622e9d00d05e49076dfb
Vejat Olea <volea19@alumnos.utalca.cl>
2024-05-15 22:59:59 -0400

    123

--- FRONTEND/src/Componentes/FormIndex.svelte
+++ FRONTEND/src/Componentes/FormIndex.svelte
@@ -38,17 +38,17 @@
 {#if rol === 'usuario'}
     <div class="flex h-screen ">
         <!-- Primer fondo blanco -->
-        <a href="" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border  relative overflow-hidden group">
+        <a href="http://localhost:4321/noticias" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border  relative overflow-hidden group">
             <img src="Noticia.jpeg" class="w-full h-full object-cover transition duration-300 transform hover:scale-105" alt="Imagen 1">
             <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Noticias</div>
         </a>
         <!-- Segundo fondo blanco -->
-        <a href="" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border  relative overflow-hidden group">
+        <a href="http://localhost:4321/FormularioForo" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border  relative overflow-hidden group">
             <img src="foroadmin.jpeg" class="w-full h-full object-cover transition duration-300 transform hover:scale-105" alt="Imagen 2">
             <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Foro</div>
         </a>
         <!-- Tercer fondo blanco -->
-        <a href="http://localhost:4321/perfil" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border  relative overflow-hidden group">
+        <a href="http://localhost:4321/user" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border  relative overflow-hidden group">
             <img src="perfil1.jpeg" class="w-full h-full object-cover transition duration-300 transform hover:scale-105" alt="Imagen 3">
             <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Perfil</div>
         </a>
@@ -56,7 +56,7 @@
 {:else if rol === 'admin'}
     <div class="flex h-screen ">
         <!-- Primer fondo blanco -->
-        <a href="" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border  relative overflow-hidden group">
+        <a href="http://localhost:4321/FormularioForo" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border  relative overflow-hidden group">
             <img src="foroadmin.jpeg" class="w-full h-full  objec.t-cover transition duration-300 transform hover:scale-105" alt="Imagen 1">
             <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Foro</div>
         </a>
@@ -66,7 +66,7 @@
             <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Perfil</div>
         </a>
         <!-- Tercer fondo blanco -->
-        <a href="URL_3" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border relative overflow-hidden group">
+        <a href="http://localhost:4321/noticias" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-light-border dark:border-dark-border relative overflow-hidden group">
             <img src="Noticia.jpeg" class="w-full h-full object-cover transition duration-300 transform hover:scale-105" alt="Imagen 3">
             <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Noticias</div>
         </a>
@@ -76,7 +76,25 @@
         <!-- Primer fondo blanco -->
         <a href="http://localhost:4321/editarequipo" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-black  relative overflow-hidden group">
             <img src="agregarjudador.jpeg" class="w-full h-full object-cover transition duration-300 transform hover:scale-105" alt="Imagen 1">
-            <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Agregar jugador</div>
+            <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Torneo</div>
+        </a>
+        <!-- Segundo fondo blanco -->
+        <a href="http://localhost:4321/Noticia" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-black  relative overflow-hidden group">
+            <img src="Noticia.jpeg" class="w-full h-full object-cover transition duration-300 transform hover:scale-105" alt="Imagen 2">
+            <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Noticias</div>
+        </a>
+        <!-- Tercer fondo blanco -->
+        <a href="http://localhost:4321/perfilequipo" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-black  relative overflow-hidden group">
+            <img src="crearEquipo.jpeg" class="w-full h-full object-cover transition duration-300 transform hover:scale-105" alt="Imagen 3">
+            <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Perfil Equipo</div>
+        </a>
+    </div>
+    {:else if rol === 'creador'}
+    <div class="flex h-screen">
+        <!-- Primer fondo blanco -->
+        <a href="http://localhost:4321/torneos" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-black  relative overflow-hidden group">
+            <img src="agregarjudador.jpeg" class="w-full h-full object-cover transition duration-300 transform hover:scale-105" alt="Imagen 1">
+            <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Torneos</div>
         </a>
         <!-- Segundo fondo blanco -->
         <a href="http://localhost:4321/creacionTorneo" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-black  relative overflow-hidden group">
@@ -86,10 +104,11 @@
         <!-- Tercer fondo blanco -->
         <a href="http://localhost:4321/agregarequipo" class="w-1/3 bg-white flex flex-col items-center justify-start border-1.5 border-r border-black  relative overflow-hidden group">
             <img src="crearEquipo.jpeg" class="w-full h-full object-cover transition duration-300 transform hover:scale-105" alt="Imagen 3">
-            <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Agregar Equipo</div>
+            <div class="absolute top-0 left-0 w-full py-2 bg-black bg-opacity-50 text-white  opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">Perfil Equipo</div>
         </a>
     </div>
 {:else}
+
     <div>
       No se reconoce el rol del usuario.
       
